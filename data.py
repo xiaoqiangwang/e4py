d5ed33f7d40ac105fb11d8bf6dc88c2820b61517
Xiaoqiang Wang <xiaoqiangwang@gmail.com>
2017-04-16 21:18:34 +0200

    add docstring

--- data.py
+++ data.py
@@ -9,6 +9,9 @@ class ArrayFlag(enum.IntEnum):
     BoundSizeArray = 0x02
     FixedSizeArray = 0x03
 
+    def __str__(self):
+        return self.name
+
 
 class DataFlag(enum.IntEnum):
     # Boolean
@@ -33,6 +36,9 @@ class DataFlag(enum.IntEnum):
     VariantUnion = 14
     BoundedString = 15
 
+    def __str__(self):
+        return self.name
+
 
 class FieldEncoding(enum.IntEnum):
     No = 0xFF
@@ -51,17 +57,121 @@ class DataRegistry(object):
     def get_type(self, id_):
         return self.registry[id_]
 
-
+# singleton
 registry = DataRegistry()
 
 
 class DataType(object):
+    """
+    Field data type.
+
+    Each Field introspection description (FieldDesc) MUST be encoded as a byte.
+
+    +---+-------+---------------------------+
+    |bit| Value | Description               |
+    +===+=======+===========================+
+    |   | 110   | reserved                  |
+    |   +-------+                           |
+    |   | 110   |                           |
+    |   +-------+                           |
+    |   | 101   |                           |
+    |   +-------+---------------------------+
+    |   | 100   | complex                   |
+    |7-5+-------+---------------------------+
+    |   | 011   | string                    |
+    |   +-------+---------------------------+
+    |   | 010   | floating-point            |
+    |   +-------+---------------------------+
+    |   | 001   | integer                   |
+    |   +-------+---------------------------+
+    |   | 000   | boolean                   |
+    +---+-------+---------------------------+
+    |   | 11    | fixed-size array          |
+    |   +-------+---------------------------+
+    |   | 10    | bounded-size array        |
+    |4-3+-------+---------------------------+
+    |   | 01    | variable-size array       |
+    |   +-------+---------------------------+
+    |   | 00    | scalar                    |
+    +---+-------+---------------------------+
+    |2-0|       | type (bits 7-5) dependant |
+    +---+-------+---------------------------+
+
+    * integer type
+
+    +---+-------+-----------+
+    |bit| Value | Type Name |
+    +===+=======+===========+
+    |   | 1     | unsigned  |
+    | 2 +-------+-----------|
+    |   | 0     | signed    |
+    +---+-------+-----------+
+    |   | 11    | long      |
+    |   +-------+-----------+
+    |   | 10    | int       |
+    |1-0+-------+-----------+
+    |   | 01    | short     |
+    |   +-------+-----------+
+    |   | 00    | byte      |
+    +---+-------+-----------+
+
+    * floating-point type
+
+    +---+-------+-----------+-----------------------+
+    |bit| Value | Type Name | IEEE 754-2008 Name    |
+    +===+=======+===========+=======================+
+    |   | 111   |                                   |
+    |   +-------+                                   |
+    |   | 110   |     reserved                      |
+    |   +-------+                                   |
+    |   | 101   |                                   |
+    |   +-------+-----------+-----------------------+
+    |   | 100   | reserved  | binary128 (Quadruple) |
+    |2-0+-------+-----------+-----------------------+
+    |   | 011   | double    | binary64 (Double)     |
+    |   +-------+-----------+-----------------------+
+    |   | 010   | float     | binary32 (Single)     |
+    |   +-------+-----------+-----------------------+
+    |   | 001   | reserved  | binary16 (Half)       |
+    |   +-------+-----------+-----------------------+
+    |   | 000   | reserved                          |
+    +---+-------+-----------------------------------+
+
+    * complex type
+
+    +---+-------+----------------+
+    |bit| Value | Type Name      |
+    +===+=======+================+
+    |   | 111   |                |
+    |   +-------+                |
+    |   | 110   |                |
+    |   +-------+ reserved       |
+    |   | 101   |                |
+    |   +-------+                |
+    |   | 100   |                |
+    |2-0+-------+----------------+
+    |   | 011   | bounded string |
+    |   +-------+----------------+
+    |   | 010   | variant union  |
+    |   +-------+----------------+
+    |   | 001   | union          |
+    |   +-------+----------------+
+    |   | 000   | structure      |
+    +---+-------+----------------+
+    """
 
     def __init__(self, *args):
         self.type_code, self.array_flag = args
 
     def __str__(self):
-        return '%s %s' % (self.type_code, self.array_flag)
+        if self.array_flag == ArrayFlag.Scalar:
+            return '%s' % self.type_code
+        elif self.array_flag == ArrayFlag.VarSizeArray:
+            return '%s[]' % self.type_code
+        elif self.array_flag == ArrayFlag.FixedSizeArray:
+            return '%s[]' % self.type_code
+        elif self.array_flag == ArrayFlag.BoundSizeArray:
+            return  '%s<>' % self.type_code
 
     def to_field_desc(self):
         array_bits = self.array_flag << 3
@@ -96,38 +206,49 @@ class DataType(object):
 
     @staticmethod
     def from_field_desc(field_desc):
+        # bit 3-4 is array flag
         array_flag = ArrayFlag((field_desc & 0x18) >> 3)
-
+        # bit 6-7 is type selection
         major_code = (field_desc & 0xE0) >> 5
-        subtype_code = field_desc & 0x07
+        # bit 0-2 is dependant on major type
+        sub_code = field_desc & 0x07
 
         if major_code == 0b000:
             type_code = DataFlag.Boolean
 
         elif major_code == 0b001: # integer
-            if subtype_code & 0b100: # unsigned
-                type_code = DataFlag(DataFlag.UByte + (subtype_code & 0b011) * 2)
+            if sub_code & 0b100: # unsigned
+                type_code = DataFlag(DataFlag.UByte + (sub_code & 0b011) * 2)
             else:
-                type_code = DataFlag(DataFlag.Byte + (subtype_code & 0b011) * 2)
+                type_code = DataFlag(DataFlag.Byte + (sub_code & 0b011) * 2)
 
         elif major_code == 0b010: # floating-point
-            if subtype_code == 0b010:
+            if sub_code == 0b010:
                 type_code = DataFlag.Float
-            elif subtype_code == 0b011:
+            elif sub_code == 0b011:
                 type_code = DataFlag.Double
 
         elif major_code == 0b011:
             type_code = DataFlag.String
 
         elif major_code == 0b100:
-            type_code = DataFlag(DataFlag.Structure + subtype_code)
+            type_code = DataFlag(DataFlag.Structure + sub_code)
 
         return DataType(type_code, array_flag)
 
 
 class DataObject(object):
+    """
+    Generic introspectional data object.
+
+    :data:`DataObject.type_` describes the basic data type, boolean, string, byte, ubyte, structure, union or array of them.
+    :data:`DataObject.size` designates the size of the data type. For structure, it is the number of fields.
+    For fixed or bounded array, it is the number of elements. For scalar type, it is always 0.
+    :data:`DataObject.fields` is a list of fields. Each field is a (*name*, :class:`DataObject`) tuple.
+
+    """
     def __init__(self, *args):
-        self.id_,  self.type_, self.size, self.fields = args
+        self.type_, self.size, self.fields = args
         self.name = b''
 
     @staticmethod
@@ -152,9 +273,8 @@ class DataObject(object):
                     for i in range(size):
                         name = buffer.get_string()
                         object_ = DataObject.from_buffer(buffer)
-                        object_.name = name
-                        fields.append(object_)
-                    object_ = DataObject(id_, data_type.type_code, size, fields)
+                        fields.append((name, object_))
+                    object_ = DataObject(data_type, size, fields)
                     object_.name = name_
                     registry.register_type(id_, object_)
                     return object_
@@ -162,13 +282,13 @@ class DataObject(object):
                     object_ = DataObject.from_buffer(buffer)
                     return object_
             elif data_type.type_code == DataFlag.VariantUnion:
-                object_ = DataObject(id_, data_type.type_code, 0, [])
+                object_ = DataObject(data_type, 0, [])
                 registry.register_type(id_, object_)
                 return object_
             elif data_type.type_code == DataFlag.BoundedString:
                 name = buffer.get_string()
                 size = buffer._get_size()
-                object_ = DataObject(id_, data_type.type_code, size, [])
+                object_ = DataObject(data_type, size, [])
                 object_.name = name
                 registry.register_type(id_, object_)
                 return object_
@@ -177,13 +297,13 @@ class DataObject(object):
             size = 0
             if data_type.array_flag == ArrayFlag.FixedSizeArray or data_type.array_flag == ArrayFlag.BoundSizeArray:
                 size = buffer._get_size()
-            return DataObject(field_enc, data_type.array_flag, size, [])
+            return DataObject(data_type, size, [])
 
 
     def __str__(self):
-        output = '%d %s %s %d' % (self.id_, self.name, self.type_, self.size)
-        for object_ in self.fields:
-            output += '\n  %s' % (object_)
+        output = '%s %s %d' % (self.name, self.type_, self.size)
+        for name, object_ in self.fields:
+            output += '\n  %s: %s' % (name, object_)
         return output
 
 if __name__ == '__main__':
@@ -192,13 +312,13 @@ if __name__ == '__main__':
     import sys
     from messages import BufferReader
 
-    desc = \
+    desc1 = \
     b"FD 00 01 80  0B 74 69 6D  65 53 74 61  6D 70 5F 74"\
     b"03 10 73 65  63 6F 6E 64  73 50 61 73  74 45 70 6F"\
     b"63 68 23 0B  6E 61 6E 6F  53 65 63 6F  6E 64 73 22"\
     b"07 75 73 65  72 54 61 67  22"
 
-    desc = \
+    desc2 = \
     b'FD 00 01 80  10 65 78 61  6D 70 6C 65  53 74 72 75'\
     b'63 74 75 72  65 07 05 76  61 6C 75 65  28 10 62 6F'\
     b'75 6E 64 65  64 53 69 7A  65 41 72 72  61 79 30 10'\
@@ -216,7 +336,7 @@ if __name__ == '__main__':
     b'65 43 0C 76  61 72 69 61  6E 74 55 6E  69 6F 6E FD'\
     b'00 05 82'
 
-    desc = b'fd0100801665706963733a6e742f4e544e4441727261793a312e30080576616c7'\
+    desc3 = b'fd0100801665706963733a6e742f4e544e4441727261793a312e30080576616c7'\
            b'565fd020081000b0c626f6f6c65616e56616c756508096279746556616c756528'\
            b'0a73686f727456616c75652908696e7456616c75652a096c6f6e6756616c75652'\
            b'b0a756279746556616c75652c0b7573686f727456616c75652d0975696e745661'\
@@ -233,7 +353,7 @@ if __name__ == '__main__':
            b'57363726970746f72600a736f75726365547970652206736f7572636560'
 
     if len(sys.argv) == 1:
-        byte = codecs.decode(desc.replace(b' ', b''), 'hex')
+        byte = codecs.decode(desc3.replace(b' ', b''), 'hex')
         hexdump.hexdump(byte)
         buffer = BufferReader(byte)
         object_ = DataObject.from_buffer(buffer)
